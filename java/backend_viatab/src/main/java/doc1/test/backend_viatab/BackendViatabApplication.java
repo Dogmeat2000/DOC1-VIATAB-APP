c02e7a6af89bdabf79588a29af1333e97a3fac34
Dogmeat2000 <86206683+Dogmeat2000@users.noreply.github.com>
2025-06-18 16:34:21 +0000

    Initial

---
+++ java/backend_viatab/src/main/java/doc1/test/backend_viatab/BackendViatabApplication.java
@@ -0,0 +1,13 @@
+package doc1.test.backend_viatab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendViatabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendViatabApplication.class, args);
+    }
+
+}
