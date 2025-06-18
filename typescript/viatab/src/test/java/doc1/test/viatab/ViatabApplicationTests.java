bd4e5a4c20d63a0913363b7ecabc26572a40f368
Dogmeat2000 <86206683+Dogmeat2000@users.noreply.github.com>
2025-06-18 15:22:44 +0000

    Initial

---
+++ typescript/viatab/src/test/java/doc1/test/viatab/ViatabApplicationTests.java
@@ -0,0 +1,13 @@
+package doc1.test.viatab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViatabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
