3412933d88a470a0b53176fe066a4985e87553dd
Dogmeat2000 <86206683+Dogmeat2000@users.noreply.github.com>
2025-06-18 20:43:19 +0000

    update

--- java/backend_viatab/src/main/java/doc1/test/backend_viatab/config/CorsConfig.java
+++ java/backend_viatab/src/main/java/doc1/test/backend_viatab/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000") // Use container name if needed
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
